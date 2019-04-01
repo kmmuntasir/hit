dd5d10dac5b961e95966dbcb8b3a029557083b26
Muntasir Billah Munna <kmmuntasir@gmail.com>
2019-04-01 20:40:48 +0600

    Renamed app, classified header files

--- hit.cpp
+++ hit.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #define MAXN 10000
@@ -6,12 +7,14 @@
 using namespace std;
 
 #include "md5.h"
+#include "directory.h"
 #include "custom_git.h"
 
 int main() {
-
     tree root;
-    root = init_tree("dir1", "dir1");
+    string initial_directory = "dir1";
+    init_app(initial_directory);
+    root = init_tree(initial_directory, initial_directory);
     tree_display(root);
     cout << endl;
     cout << stringify_tree(root) << endl << endl << endl;
